7a148ce7f8b467cd196c43e53891d3485fe515a2
adminnirobvai1-ux <adminnirobvai1@gmail.com>
2026-09-23 19:05:31 -0700

    Update main.py

--- main.py
+++ main.py
@@ -107,7 +107,7 @@ def cleanup_zombie_browsers():
 # ==========================================
 # 4. Configuration & State Management
 # ==========================================
-TOKEN = "8808949150:AAGJXBVM_18xxAHPFMtH-TnGQbtaXAcb9p8"
+TOKEN = "8808949150:AAEQk9wSLZqYRYhXS6E5xUKTv9JM8MJ0vFo"
 bot = telebot.TeleBot(TOKEN, parse_mode="HTML")
 
 HEADLESS_MODE = os.environ.get("HEADLESS", "true").lower() == "true"
